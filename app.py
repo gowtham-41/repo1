34ae67a634d4351260b24a12579d129daceab361
gowtham-41 <124654525+gowtham-41@users.noreply.github.com>
2023-08-24 15:23:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import *
+import ibm_db
+
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=9938aec0-8105-433e-8bf9-0fbb7e483086.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32459;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=llg82607;PWD=4ynMFVzoX22U1ymb",'','')
+print(conn)
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login1',methods=['POST'])
+def login1():
+    NAME = request.form['NAME']
+    EMAIL = request.form['EMAIL']
+    sql = "SELECT * FROM REGISTER WHERE NAME =? AND EMAIL=?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,NAME)
+    ibm_db.bind_param(stmt,2,EMAIL)
+    ibm_db.execute(stmt)
+    account = ibm_db.fetch_assoc(stmt)
+    print (account)
+    print(NAME,EMAIL)
+    if account:
+            return render_template('login.html', pred="Login successful")
+    else:
+        return render_template('register.html', pred="Login unsuccessful. Please register!") 
+
+
+@app.route('/register1',methods=['POST'])
+def register1():
+    x = [x for x in request.form.values()]
+    print(x)
+    NAME=x[0]
+    EMAIL=x[1]
+    PASSWORD=x[2]
+    sql = "SELECT * FROM REGISTER WHERE EMAIL =?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,EMAIL)
+    ibm_db.execute(stmt)
+    account = ibm_db.fetch_assoc(stmt)
+    print(account)
+    if account:
+        return render_template('login.html', pred="You are already a member, please login using your details")
+    else:
+        insert_sql = "INSERT INTO  REGISTER VALUES (?, ?, ?)"
+        prep_stmt = ibm_db.prepare(conn, insert_sql)
+        ibm_db.bind_param(prep_stmt, 1, NAME)
+        ibm_db.bind_param(prep_stmt, 2, EMAIL)
+        ibm_db.bind_param(prep_stmt, 3, PASSWORD)
+        ibm_db.execute(prep_stmt)
+        return render_template('login.html', pred="Registration Successful, please login using your details")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port = 5000)
